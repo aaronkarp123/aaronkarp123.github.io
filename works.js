f825ed065e05d4e6bc03447b524e334b00017d2b
aaronkarp123 <aaronkarp123@gmail.com>
2018-09-03 10:14:04 -0400

    Streamlined works.js and added mobile site

---
+++ works.js
@@ -0,0 +1,8 @@
+var music_w = ['Tare (String Quartet, 3 Sopranos, Reel-to-Reel) 2018', 'Traces (Animation Sound Design) 2018', 'Introduction (Fixed Media) 2018', 'Retrained Form (Installation) 2017'];
+var music_wl = ['https://soundcloud.com/aaronkarp/Tare', 'https://www.youtube.com/watch?v=U1ws99j5U8Q', 'https://soundcloud.com/aaronkarp/introduction', 'https://youtu.be/1gMaHXmhG6U'];
+
+var cs_w = ['HaptEQ: A collaborative tool for visually impaired audio producers', 'An Approach to Audio-Only Editing for Visually Impaired Seniors'];
+var cs_wl = ['https://www.youtube.com/watch?v=gminuhZIVOo&feature=youtu.be', 'https://dl.acm.org/citation.cfm?id=2982196'];
+
+var other_w = ['About Me', 'Live Concert Graphics'];
+var other_wl = ['about.html', 'https://github.com/aaronkarp123/visuals'];
