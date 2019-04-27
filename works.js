a83639ac54baee2251f3f2cf36889475726ad6c0
aaronkarp123 <aaronkarp123@gmail.com>
2019-04-27 13:47:45 -0400

    WOrking Fade

--- works.js
+++ works.js
@@ -1,8 +1,8 @@
 var music_w = ['with in (Installation) 2018','Tare (String Quartet, 3 Sopranos, Reel-to-Reel) 2018', 'Traces (Animation Sound Design) 2018', 'Introduction (Fixed Media) 2018', 'Retrained Form (Installation) 2017'];
-var music_wl = ['within.html', 'https://soundcloud.com/aaronkarp/Tare', 'https://www.youtube.com/watch?v=U1ws99j5U8Q', 'https://soundcloud.com/aaronkarp/introduction', 'https://youtu.be/1gMaHXmhG6U'];
+var music_wl = ['https://aaronmkarp.com/within.html', 'https://aaronmkarp.com/tare.html', 'https://www.youtube.com/embed/U1ws99j5U8Q?enablejsapi=1', 'https://aaronmkarp.com/introduction.html', 'https://www.youtube.com/embed/1gMaHXmhG6U?enablejsapi=1'];
 
 var cs_w = ['HaptEQ: A collaborative tool for visually impaired audio producers', 'An Approach to Audio-Only Editing for Visually Impaired Seniors'];
-var cs_wl = ['https://www.youtube.com/watch?v=gminuhZIVOo&feature=youtu.be', 'https://dl.acm.org/citation.cfm?id=2982196'];
+var cs_wl = ['https://www.youtube.com/embed/gminuhZIVOo?enablejsapi=1', 'https://dl.acm.org/citation.cfm?id=2982196'];
 
 var other_w = ['Falter/Cleanse (Solo Viola and Electronics) 2018 - composed by Hunter Brown', 'CV', 'Github'];
 var other_wl = ['https://soundcloud.com/hunter-brown-music/faltercleanse', 'files/AaronKarpCV.pdf', 'https://github.com/aaronkarp123'];
