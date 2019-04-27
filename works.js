7863b55dacd2af2e330b3ff7bb8b4bbe4c5f34c1
aaronkarp123 <aaronkarp123@gmail.com>
2019-04-27 12:02:44 -0400

    trying

--- works.js
+++ works.js
@@ -1,5 +1,5 @@
 var music_w = ['with in (Installation) 2018','Tare (String Quartet, 3 Sopranos, Reel-to-Reel) 2018', 'Traces (Animation Sound Design) 2018', 'Introduction (Fixed Media) 2018', 'Retrained Form (Installation) 2017'];
-var music_wl = ['within.html', 'https://soundcloud.com/aaronkarp/Tare', 'https://www.youtube.com/watch?v=U1ws99j5U8Q', 'https://soundcloud.com/aaronkarp/introduction', 'https://youtu.be/1gMaHXmhG6U'];
+var music_wl = ['https://aaronmkarp.com/within.html', 'https://soundcloud.com/aaronkarp/Tare', 'https://www.youtube.com/watch?v=U1ws99j5U8Q', 'https://soundcloud.com/aaronkarp/introduction', 'https://youtu.be/1gMaHXmhG6U'];
 
 var cs_w = ['HaptEQ: A collaborative tool for visually impaired audio producers', 'An Approach to Audio-Only Editing for Visually Impaired Seniors'];
 var cs_wl = ['https://www.youtube.com/watch?v=gminuhZIVOo&feature=youtu.be', 'https://dl.acm.org/citation.cfm?id=2982196'];
