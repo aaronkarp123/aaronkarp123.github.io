f8627e2bb54db563dd70822a442d5eeab0239dcb
aaronkarp123 <aaronkarp123@gmail.com>
2019-12-10 12:43:15 +0900

    Typo

--- pages/aaron/data.js
+++ pages/aaron/data.js
@@ -1,7 +1,7 @@
 var title = 'Aaron Karp';
 
 var description = 'I\'m a sound artist, composer, and computer scientist currently living in Kyoto, Japan. My interests generally follow questions of technology\'s ability to mediate or hinder human cooperation and coexistence.';
-description += 'I\'ve investigated those questions through the lenses of computer science research, music compositions, installation art pieces, and combinations of all three.'
+description += ' I\'ve investigated those questions through the lenses of computer science research, music compositions, installation art pieces, and combinations of all three.'
 description += ''
 
 var specs = '~~Highlights~~<br>1995: Born in Lexington, Kentucky<br>2009: Bar mitzvah<br>2017: B.S. in Music and Computer Science, Northwestern University<br>2019: M.A. in Digital Musics, Dartmouth College<br>'
