4e3e7066df24b3f23549f4e34a367af56070b824
aaronkarp123 <aaronkarp123@gmail.com>
2019-12-15 21:25:01 +0900

    New Piece

---
+++ pages/givespace1/data.js
@@ -0,0 +1,14 @@
+var title = 'Give Space [I]';
+
+var description = 'A semi-structured improvisational piece.<br><br>Always leave room for growth.';
+
+var specs = 'Form: Live Performance<br><br>Completed: 2019<br><br>Instrumentation: Cello, Electronics<br><br>Sample Recording: Viola<br>Performer: Aaron Karp';
+
+var images = [];
+
+var pdf = 'https://aaronmkarp.com/files/GiveSpace1.pdf';
+
+var soundcloud_link = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/728131531&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true';
+
+var video = '';
+
