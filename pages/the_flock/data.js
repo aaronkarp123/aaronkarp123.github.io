7d39c5b2e3e567091c380d4ff60be48301947af8
aaronkarp123 <aaronkarp123@gmail.com>
2022-03-30 19:47:10 -0400

    Added The Flock.. documentation

---
+++ pages/the_flock/data.js
@@ -0,0 +1,14 @@
+var title = 'The Flock Takes Shelter in the Storm';
+
+var description = 'A soundscape collage, using recordings from hikes in Western Massachussetts and Northern California, and excerpts of a piece for solo cello performed by Benjamin Karp.<br><br>';
+
+var specs = 'Form: Installation<br><br>Completed: 2022<br><br>Materials: Glass, Transducers<br><br>Components: Raspberry Pi, Amplifier, Transducers x 2<br><br>Installed: January, 2022, Granoff Center for the Creative Arts<br>Providence, RI';
+
+var pdf = '';
+
+var soundcloud_link = '';
+
+var images = [];
+
+var video = "https://www.youtube.com/embed/arOtxkbTRgg";
+
