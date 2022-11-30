bdc6c44a56a28adbd0cc2304a2409f137b4e9da0
aaronkarp123 <aaronkarp123@gmail.com>
2022-11-29 20:58:04 -0500

    Added PolarCV Page

---
+++ pages/polarcv/data.js
@@ -0,0 +1,14 @@
+var title = 'Polar CV';
+
+var description = 'TBD<br>Source code available on <a href="https://github.com/aaronkarp123/EarthTones">Github</a>.<br>Module available for download from the <a href="https://library.vcvrack.com/plugins?query=&license=&sort=buildTimestamp">VCV Rack Plugin Library</a>.';
+
+var specs = 'Form: Installation<br><br>Completed: 2020<br><br>Materials: Record Player, Uncut Vinyl, Brush, String<br><br>Installed: January, 2020, Kyoto University of Art and Design<br>Kyoto, Japan';
+
+var images = ['0.png', '1.png', '2.png', '3.png', '4.png', '5.png', '6.png'];
+
+var pdf = '';
+
+var soundcloud_link = '';
+
+var video = 'https://www.youtube.com/embed/iNLZMcmS4eI';
+
---
+++ worksv12.js
@@ -0,0 +1,34 @@
+var cs_w = ['HaptEQ: A collaborative tool for visually impaired audio producers', 'An Approach to Audio-Only Editing for Visually Impaired Seniors'];
+var cs_wl = ['https://www.youtube.com/embed/gminuhZIVOo?enablejsapi=1', 'https://dl.acm.org/citation.cfm?id=2982196'];
+
+var fun_w = ['mountains', 'dial', 'u+i']
+var fun_wl = ['mountains.html', 'dial.html', 'u_i.html']
+
+var other_w = ['Prying Ears: A Critical Response to Mass Audio Surveillance (Master\'s Thesis) 2019','Falter/Cleanse (Solo Viola and Electronics) 2018 - by Hunter Brown', 'CV', 'Github'];
+var other_wl = ['files/KarpThesis.pdf','https://soundcloud.com/hunter-brown-music/faltercleanse', 'files/AaronKarpCV.pdf', 'https://github.com/aaronkarp123'];
+
+
+
+
+var sounds_name = ['The Flock Takes Shelter in the Storm',				'4 Ways to Get Home',								'Read / Write', 	'風は渡る', 		'Give Space [I]',		'with in',			'Tare', 									'Traces', 						'Introduction', 	'Restrained Form'];
+var sounds_desc = ['Installation',										'EP',												'Installation',	'Installation',	'Cello, Electronics',	'Installation',  	'String Quartet, 3 Sopranos, Reel-to-Reel', 'Animation Sound Design', 		'Fixed Media',		'Installation'];
+var sounds_year = ['2022',												'2020',												'2020',			'2019',			'2019',					'2018',				'2018',										'2018',							'2018',				'2017'];
+var sounds_folder = ['the_flock',										'4ways',											'read_write',	'kaze',		'givespace1',			'within', 			'tare', 									'traces', 						'introduction',		'restrained_form'];
+var sounds_link = ['',												    'https://oddegg.bandcamp.com/album/4-ways-to-get-home','',				'',				'',						'', 					'', 									'https://youtu.be/U1ws99j5U8Q',	'', 				''];
+
+var cs_name = ['PolarCV', 			'Prying Ears: Master\'s Thesis',								'HaptEQ', 														'Audio-Only Editing',											'Github'];
+var cs_desc = ['VCV Rack Module', 	'A Critical Response to Mass Audio Surveillance',				'A collaborative tool for visually impaired audio producers', 	'An Approach to Audio-Only Editing for Visually Impaired Seniors','Open-source code'];
+var cs_year = ['2022', 				'2019',														'2017', 															'2016', 														''];
+var cs_folder = ['polarcv', 		'pryingears',												'hapteq', 														'aoe', 															'github'];
+var cs_link = ['',					'files/KarpThesis.pdf',										'', 																'https://dl.acm.org/citation.cfm?id=2982196', 					'https://github.com/aaronkarp123'];
+
+var other_name = ['CV' 					];
+var other_desc = ['What I\'ve been up to'];
+var other_year = [''];
+var other_folder = ['cv'];
+var other_link = ['files/AaronKarpCV.pdf'];
+
+var fun_name = ['waves',		'bloom', 		'mountains', 		'dial', 	'u+i'];
+var fun_folder = ['waves',		'bloom',		'mountains',		'dial', 	'u_i'];
+var fun_link = ['waves.html',	'bloom.html',	'mountains.html', 	'dial.html','u_i.html']
+
