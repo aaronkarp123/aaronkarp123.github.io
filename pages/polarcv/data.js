993832278d16796d39a230ceecfb725c09e2a4bb
aaronkarp123 <aaronkarp123@gmail.com>
2022-12-19 16:05:33 -0700

    Updated polarCV

--- pages/polarcv/data.js
+++ pages/polarcv/data.js
@@ -1,8 +1,10 @@
 var title = 'Polar CV';
 
-var description = 'TBD<br>Source code available on <a href="https://github.com/aaronkarp123/EarthTones">Github</a>.<br>Module available for download from the <a href="https://library.vcvrack.com/plugins?query=&license=&sort=buildTimestamp">VCV Rack Plugin Library</a>.';
+var description = 'This is a VCV Rack module that generates control voltages that correspond to a point traveling along various polar equations.';
+description += '<br><br>Source code available on <a href="https://github.com/aaronkarp123/EarthTones" target="_blank">Github</a>.<br>Module available for download from the <a href="https://library.vcvrack.com/AaronKarp-EarthTones/PolarCV" target="_blank">VCV Rack Plugin Library</a>.';
+description += '<br><br>Below are some examples of different equations that can be created with this module.';
 
-var specs = 'Form: Installation<br><br>Completed: 2020<br><br>Materials: Record Player, Uncut Vinyl, Brush, String<br><br>Installed: January, 2020, Kyoto University of Art and Design<br>Kyoto, Japan';
+var specs = '';//'Form: Installation<br><br>Completed: 2020<br><br>Materials: Record Player, Uncut Vinyl, Brush, String<br><br>Installed: January, 2020, Kyoto University of Art and Design<br>Kyoto, Japan';
 
 var images = ['0.png', '1.png', '2.png', '3.png', '4.png', '5.png', '6.png'];
 
@@ -10,5 +12,5 @@ var pdf = '';
 
 var soundcloud_link = '';
 
-var video = 'https://www.youtube.com/embed/iNLZMcmS4eI';
+var video = '';
 
