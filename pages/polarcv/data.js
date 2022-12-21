81a89cde9b385d6eb2e17d9803fff5133a057d83
aaronkarp123 <aaronkarp123@gmail.com>
2022-12-21 12:38:29 -0700

    Updated polarcv page

--- pages/polarcv/data.js
+++ pages/polarcv/data.js
@@ -9,7 +9,7 @@ description += '<br><br>Below are some examples of different equations that can 
 
 
 var specs = '';//'Form: Installation<br><br>Completed: 2020<br><br>Materials: Record Player, Uncut Vinyl, Brush, String<br><br>Installed: January, 2020, Kyoto University of Art and Design<br>Kyoto, Japan';
-specs += '<br><br>Source code available on <a href="https://github.com/aaronkarp123/EarthTones" target="_blank">Github</a>.<br><br>Module available for download from the <a href="https://library.vcvrack.com/AaronKarp-EarthTones/PolarCV" target="_blank">VCV Rack Plugin Library</a>.';
+specs += 'Source code available on <a href="https://github.com/aaronkarp123/EarthTones" target="_blank">Github</a>.<br><br>Module available for download from the <a href="https://library.vcvrack.com/AaronKarp-EarthTones/PolarCV" target="_blank">VCV Rack Plugin Library</a>.';
 
 var images = ['0.png', '1.png', '2.png', '3.png', '4.png', '5.png', '6.png'];
 
